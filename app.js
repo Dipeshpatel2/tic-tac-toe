80aa9234269dc4962243eb9e4a3381018c5d03d4
Dipeshpatel2e <dipeshpatel2@yahoo.co.uk>
2023-04-12 14:19:50 +0100

    handleRestart button working

--- app.js
+++ app.js
@@ -14,37 +14,51 @@ const winningSequence = [
 ];
 
 //This line of code creates an array of 9 placeholders. An array of empty strings
-let choice = [""] * 9
+let choice = [""] * 9;
 let currentPlayer = "X";
 let running = false;
 
-console.log(boxes)
 //Functions that will be needed
-
-//handle click function- creates a function that will access each index position in the choice array
-const handleClickOnBox = (event) => {
-  if(event.target.innerText == ""){
-    event.target.innerText = currentPlayer
-    currentPlayer = (currentPlayer == "X") ? "O" : "X"
-  }
-  
-}
 
 // const startingGame
 const startGame = () => {
-    boxes.forEach((box) => box.addEventListener("click", handleClickOnBox));
-    restartButton.addEventListener("click,", handleRestart);
-    //running = true;
-  };
+  boxes.forEach((box) => box.addEventListener("click", handleClickOnBox));
+  restartButton.addEventListener("click", handleRestart);
+  //running = true;
+};
+
+
+//handle click function- creates a function that will access each index position in the choice array
+const handleClickOnBox = (event) => {
+  const id = event.target.id;
+
+  if (!choice[id]) {
+    choice[id] = currentPlayer;
+    event.target.innerText = currentPlayer;
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
+  }
+};
+// Below code working and done with help of RH
+//   if(event.target.innerText == ""){
+//     event.target.innerText = currentPlayer
+//     currentPlayer = (currentPlayer == "X") ? "O" : "X"
+//   }
+
+// }
 //const updateBox
-
 // const swapPlayer
-
 // const checkWinner
 
-const handleRestart = () =>{
-    choice = ["", "", "", "", "", "", "", "",];
-    boxes.forEach(box => event.target.innerText = "");
-}
-//Add event listeners
-startGame()
+//Restart game function
+
+const handleRestart = () => {
+  choice = choice = [""] * 9;
+
+  boxes.forEach((box) => {
+    box.innerText = "";
+  });
+
+  currentPlayer = "X";
+};
+
+startGame();
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -7,34 +7,43 @@ var winningSequence = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2
 
 var choice = [""] * 9;
 var currentPlayer = "X";
-var running = false;
-console.log(boxes); //Functions that will be needed
-//handle click function- creates a function that will access each index position in the choice array
-
-var handleClickOnBox = function handleClickOnBox(event) {
-  if (event.target.innerText == "") {
-    event.target.innerText = currentPlayer;
-    currentPlayer = currentPlayer == "X" ? "O" : "X";
-  }
-}; // const startingGame
-
+var running = false; //Functions that will be needed
+// const startingGame
 
 var startGame = function startGame() {
   boxes.forEach(function (box) {
     return box.addEventListener("click", handleClickOnBox);
   });
-  restartButton.addEventListener("click,", handleRestart); //running = true;
-}; //const updateBox
+  restartButton.addEventListener("click", handleRestart); //running = true;
+}; //handle click function- creates a function that will access each index position in the choice array
+
+
+var handleClickOnBox = function handleClickOnBox(event) {
+  var id = event.target.id;
+
+  if (!choice[id]) {
+    choice[id] = currentPlayer;
+    event.target.innerText = currentPlayer;
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
+  }
+}; // Below code working and done with help of RH
+//   if(event.target.innerText == ""){
+//     event.target.innerText = currentPlayer
+//     currentPlayer = (currentPlayer == "X") ? "O" : "X"
+//   }
+// }
+//const updateBox
 // const swapPlayer
 // const checkWinner
+//Restart game function
 
 
 var handleRestart = function handleRestart() {
-  choice = ["", "", "", "", "", "", "", ""];
+  choice = choice = [""] * 9;
   boxes.forEach(function (box) {
-    return event.target.innerText = "";
+    box.innerText = "";
   });
-}; //Add event listeners
-
+  currentPlayer = "X";
+};
 
 startGame();
